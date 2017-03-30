228028fe1c2e1fafb1e5df31dc158df971ac9914
Alexandr Cherednichenko <Aleksandr.Cherednichenko@waveaccess.ru>
2017-03-30 13:08:42 +0300

    Improved height calculation. To find out height we need wait, while all images are loaded.

--- MailPusher/Scripts/common/emailHelper.js
+++ MailPusher/Scripts/common/emailHelper.js
@@ -4,9 +4,15 @@
     $("#emailSubject").html(email.subjectLine);
     $.get(emailBodyURL, { emailId: email.id }).done(function (data) {
         $("#emailBody").contents().find("html").html(data);
-        $("#emailBody").height($("#emailBody").contents().find("html").height());
+        updateIframeHeight();
+        setTimeout( function () { updateIframeHeight(); }, 1000);
     });
     $("#emailData").show();
+}
+
+function updateIframeHeight()
+{
+    $("#emailBody").height($("#emailBody").contents().find("html").height());
 }
 
 function hideSingleEmail() {
