897593282e77d06aeaa6eee207bcf7379f751957
Alexandr Cherednichenko <Aleksandr.Cherednichenko@waveaccess.ru>
2017-03-30 12:36:48 +0300

    fixed - Style error when viewing an email div changed to iframe

--- MailPusher/Scripts/common/emailHelper.js
+++ MailPusher/Scripts/common/emailHelper.js
@@ -3,7 +3,8 @@
     $("#emailReceivedGMT").html(email.receivedGMT);
     $("#emailSubject").html(email.subjectLine);
     $.get(emailBodyURL, { emailId: email.id }).done(function (data) {
-        $("#emailBody").html(data);
+        $("#emailBody").contents().find("html").html(data);
+        $("#emailBody").height($("#emailBody").contents().find("html").height());
     });
     $("#emailData").show();
 }
