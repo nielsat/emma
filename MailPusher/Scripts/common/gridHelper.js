02160773bab92dcc94badcf8c1792cc257be65b3
Alexandr Cherednichenko <Aleksandr.Cherednichenko@waveaccess.ru>
2017-04-02 21:52:33 +0300

    Grid issues #39

---
+++ MailPusher/Scripts/common/gridHelper.js
@@ -0,0 +1,10 @@
+﻿var gridHelper = {
+    updateContainerHeight: function (containerId, gridId, minHeight) {
+        var newHeight = minHeight + $("#" + gridId).height();
+        $("#" + containerId).height(newHeight);
+    },
+    changeTdCursorToPointer: function (tableId, exceptionClass) {
+        $("#" + tableId).find('td').css("cursor", "pointer");
+        $("." + exceptionClass).parents('td').css("cursor", "default");
+    }
+}
