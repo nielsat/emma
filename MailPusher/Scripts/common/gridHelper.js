f7e9637319aeb455ddbbd092d3daa33bc01d34ad
Alexandr Cherednichenko <Aleksandr.Cherednichenko@waveaccess.ru>
2017-04-11 16:42:47 +0300

    Most pages: Remove previous / next controls #25

--- MailPusher/Scripts/common/gridHelper.js
+++ MailPusher/Scripts/common/gridHelper.js
@@ -6,5 +6,10 @@
     changeTdCursorToPointer: function (tableId, exceptionClass) {
         $("#" + tableId).find('td').css("cursor", "pointer");
         $("." + exceptionClass).parents('td').css("cursor", "default");
+    },
+    hidePginationIfOnePage: function (tableID, oSettings) {
+        if (oSettings._iDisplayLength > oSettings.fnRecordsDisplay()) {
+            $(oSettings.nTableWrapper).find('.dataTables_paginate').hide();
+        }
     }
 }
