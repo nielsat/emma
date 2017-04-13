c3ac8842110096895b2808d5bcbdde48804deba2
Alexandr Cherednichenko <Aleksandr.Cherednichenko@waveaccess.ru>
2017-04-13 17:45:38 +0300

    Fix - view when no data + customization

--- MailPusher/Scripts/common/chartHelper.js
+++ MailPusher/Scripts/common/chartHelper.js
@@ -1,39 +1,36 @@
 ﻿var chartHelper = {
     drawLineTimeChart : function(sourceData, chartContainerId, titleId)
     {
-        console.log(1);
         $("#" + titleId).html(sourceData.Title);
-        console.log(2);
         google.charts.load('current', { 'packages': ['line', 'corechart'] });
-        console.log(3);
         google.charts.setOnLoadCallback(drawLTChart);
-        console.log(4);
 
         function drawLTChart() {
-            console.log(5);
+            if (!sourceData.Line.Points.length || sourceData.Line.Points.length == 0)
+            {
+                $("#" + chartContainerId).html("No data");
+            }
             var chartDiv = document.getElementById(chartContainerId);
-            console.log(6);
 
             var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Date');
+            data.addColumn('string', '');
             data.addColumn('number', sourceData.Line.Title);
-            console.log(7);
             var tmpData = [];
             for (var i = 0; i < sourceData.Line.Points.length; i++)
             {
                 var currentPoint = sourceData.Line.Points[i];
                 tmpData.push([currentPoint.StringDate, currentPoint.Value]);
             }
-            console.log(8);
             data.addRows(tmpData);
 
-            var materialOptions = {
-                height: 500
+            var options = {
+                height: 500,
+                pointSize: 10,
+                dataOpacity: 1,
+                vAxis: { minValue: 0, maxValue: 100 },
             };
-            console.log(9);
-            var materialChart = new google.charts.Line(chartDiv);
-            materialChart.draw(data, materialOptions);
-            console.log(10);
+            var materialChart = new google.visualization.LineChart(chartDiv);
+            materialChart.draw(data, options);
         }
     }
 }
